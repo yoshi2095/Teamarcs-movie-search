1e271d98a8712cb448852f8f0b0297c87866e962
yoshi2095 <arora4.yoshi@gmail.com>
2020-08-31 20:00:06 +0530

    Updates

---
+++ precache-manifest.689cf3faee73ffa1413eded7789e7ca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15650a8949c2d185361aaaa97fd95def",
+    "url": "/Teamarcs-movie-search/index.html"
+  },
+  {
+    "revision": "fe6feaf47c7407c1d85c",
+    "url": "/Teamarcs-movie-search/static/css/main.9af42ad5.chunk.css"
+  },
+  {
+    "revision": "e4c6d749caf89d643d97",
+    "url": "/Teamarcs-movie-search/static/js/2.50bab07b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Teamarcs-movie-search/static/js/2.50bab07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6feaf47c7407c1d85c",
+    "url": "/Teamarcs-movie-search/static/js/main.d8c2578e.chunk.js"
+  },
+  {
+    "revision": "fdaa07b9b18ea8b33774",
+    "url": "/Teamarcs-movie-search/static/js/runtime-main.35396ebf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Teamarcs-movie-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
